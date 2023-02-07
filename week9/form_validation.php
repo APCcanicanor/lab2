8fbedd2cd250ce8129f7d50979ec441ef8dfea54
APCcanicanor <canicanor@student.apc.edu.ph>
2023-02-07 15:19:42 +0800

    connect (6)

--- week9/form_validation.php
+++ week9/form_validation.php
@@ -1,12 +1,12 @@
-<!DOCTYPE HTML>  
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<style>
-.error {color: #FF0000;}
-</style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
-<body>  
-
+<body>
 <?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
@@ -128,7 +128,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	
 }
 ?>
-
-
 </body>
 </html>
